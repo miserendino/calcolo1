fd532eca6a158e45897ceb05357bdce6cb4932ad
Massimo <thgdfthrplay@gmail.com>
2015-12-05 12:19:30 +0100

    order almost everything

--- bar.cpp
+++ bar.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include "lib.hpp"
-#include <fstream>
-#define NN 100
-int main(){
+#include<cmath>
+#include<fstream>
 
-          std::ofstream output;
-          output.open("dati.dat");
-          for(int i=0;i<NN;++i){
-            output << i << std::endl;
-          }
-  return 0;
+ 
+int main(){
+double x[200],y[200];
+  loadR2(x,y,200);
+bubblesortx2dimcr(x,y,200);
+std::cout << "Ecco le coppie ordinate in ordine crecente rispetto alle x" << std::endl;
+for(int i=0;i<200;++i){
+  std::cout << x[i] << " " << y[i] << std::endl;
+}
+bubblesortx2dimdcr(x,y,200);
+std::cout << "Ecco le coppie ordinate in ordine decrecente rispetto alle x" << std::endl;
+for(int i=0;i<200;++i){
+  std::cout << x[i] << " " << y[i] << std::endl;
+}
+#if 0
+bubblesorty2dimdcr(x,y,200);
+std::cout << "Ecco le coppie ordinate in ordine decrecente rispetto alle y" << std::endl;
+for(int i=0;i<200;++i){
+  std::cout << x[i] << " " << y[i] << std::endl;
+}
+bubblesorty2dimcr(x,y,200);
+std::cout << "Ecco le coppie ordinate in ordine crecente rispetto alle y" << std::endl;
+for(int i=0;i<200;++i){
+  std::cout << x[i] << " " << y[i] << std::endl;
+}
+#endif
+return 0;
 }
